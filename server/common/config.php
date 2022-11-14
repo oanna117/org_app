40b79fc534381a735e1024109acfef90d52eb99b
oanna117 <o.anan117@gmail.com>
2022-11-14 11:52:16 +0900

    テーブル作成中3

--- server/common/config.php
+++ server/common/config.php
@@ -2,6 +2,6 @@
 
 <?php
 // 接続に必要な情報を定数として定義
-define('DSN', 'mysql:host=db;dbname=photo_db;charset=utf8');
-define('USER', 'calendar_admin');
+define('DSN', 'mysql:host=db;dbname=org_app_db;charset=utf8');
+define('USER', 'org_app_admin');
 define('PASSWORD', '1234');
